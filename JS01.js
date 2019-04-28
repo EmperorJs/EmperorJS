59be183d1d6ddba933a91a74362d5c16195feb98
EmperorJs <50064968+EmperorJs@users.noreply.github.com>
2019-04-28 12:05:03 +0800

    My Class Project

---
+++ JS01.js
@@ -0,0 +1,12 @@
+$('.dropdown-toggle').click(function(e) {
+    if ($(document).width() > 768) {
+      e.preventDefault();
+  
+      var url = $(this).attr('href');
+  
+      if (url !== '#') {
+        window.location.href = url;
+      }
+  
+    }
+  });
